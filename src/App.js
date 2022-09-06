fb190ae67fa7b9de285b99ca98d7384c1a46f303
BetterLogicTeam <betterlogicslhr@gmail.com>
2022-09-05 23:29:40 -0700

    last commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Components/Header/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Main_home from './Components/Main_home/Main_home';
+import Pool from './Components/Pool/Pool';
+import Footer from './Components/Footer/Footer';
+
+
+function App() {
+  return (
+    <div className="App">
+     <Header/>
+     <Main_home/>
+     <Pool/>
+     <Footer/>
+    
+    </div>
+  );
+}
+
+export default App;
